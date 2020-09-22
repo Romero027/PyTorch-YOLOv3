65b4aa3e6fa09e9754e24e29647d7b97c3e558ed
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-09-22 14:36:36 -0400

    add detr

---
+++ detr.py
@@ -0,0 +1,161 @@
+from __future__ import division
+
+from models import *
+from utils.utils import *
+from utils.datasets import *
+
+import os
+import sys
+import time
+import datetime
+import argparse
+import pickle
+
+from PIL import Image
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib.ticker import NullLocator
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
+    parser.add_argument("--resnet_model", type=str, default="resnet50", help="resnet model to use")
+    parser.add_argument("--class_path", type=str, default="data/coco.names", help="path to class label file")
+    parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
+    parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
+    parser.add_argument("--batch_size", type=int, default=1, help="size of the batches"))
+    opt = parser.parse_args()
+    print(opt)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    os.makedirs("output", exist_ok=True)
+
+    # Set up model
+    print("Using detection model with {opt.resnet_model}")
+    if opt.resnet_model == 'resnet101':
+        model = th.hub.load('facebookresearch/detr', 'detr_resnet101', pretrained=True)
+    elif opt.resnet_model = 'resnet50':
+        model = th.hub.load('facebookresearch/detr', 'detr_resnet50', pretrained=True)
+
+    model.eval()
+    model = model.cuda()
+
+    dataloader = DataLoader(
+        ImageFolder(opt.image_folder, img_size=opt.img_size),
+        batch_size=opt.batch_size,
+        shuffle=False,
+    )
+
+    CLASSES = [
+        'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+        'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+        'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+        'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+        'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+        'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+        'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+        'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+        'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+        'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+        'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+        'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+        'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+        'toothbrush'
+    ]
+
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+    imgs = []  # Stores image paths
+    img_detections = []  # Stores detections for each image index
+
+    print("\nPerforming object detection:")
+    prev_time = time.time()
+    for batch_i, (img_paths, input_imgs) in enumerate(dataloader):
+        # Configure input
+        input_imgs = Variable(input_imgs.type(Tensor))
+
+        # Get detections
+        with torch.no_grad():
+            detections = model(input_imgs)
+
+        # Log progress
+        current_time = time.time()
+        inference_time = datetime.timedelta(seconds=current_time - prev_time)
+        prev_time = current_time
+        print("\t+ Batch %d, Inference Time: %s" % (batch_i, inference_time))
+
+        # Save image and detections
+        imgs.extend(img_paths)
+        img_detections.extend(detections)
+
+
+    # print("\nSaving images:")
+    # inference_dict = {}
+    # # Iterate through images and save plot of detections
+    # for img_i, (path, detections) in enumerate(zip(imgs, img_detections)):
+
+    #     print("(%d) Image: '%s'" % (img_i, path))
+
+    #     # Get the image name
+    #     img_name = os.path.splitext(os.path.basename(path))[0]
+    #     inference_dict[img_name] = []
+
+    #     # Create plot
+    #     img = np.array(Image.open(path))
+    #     plt.figure()
+    #     fig, ax = plt.subplots(1)
+    #     ax.imshow(img)
+
+    #     # Draw bounding boxes and labels of detections
+    #     if detections is not None:
+    #         # Rescale boxes to original image
+    #         detections = rescale_boxes(detections, opt.img_size, img.shape[:2])
+    #         unique_labels = detections[:, -1].cpu().unique()
+    #         n_cls_preds = len(unique_labels)
+    #         bbox_colors = random.sample(colors, n_cls_preds)
+    #         for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
+
+    #             print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
+
+    #             box_w = x2 - x1
+    #             box_h = y2 - y1
+
+    #             # save inference results
+    #             inference_dict[img_name].append({
+    #                 'label': classes[int(cls_pred)],
+    #                 'score': cls_conf.item(),
+    #                 'box': [x1.item(), y1.item(), box_w.item(), box_h.item()]
+    #             })
+
+    #             color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
+    #             # Create a Rectangle patch
+    #             bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor="none")
+    #             # Add the bbox to the plot
+    #             ax.add_patch(bbox)
+    #             # Add label
+    #             plt.text(
+    #                 x1,
+    #                 y1,
+    #                 s=classes[int(cls_pred)],
+    #                 color="white",
+    #                 verticalalignment="top",
+    #                 bbox={"color": color, "pad": 0},
+    #             )
+
+    #     # Save generated image with detections
+    #     plt.axis("off")
+    #     plt.gca().xaxis.set_major_locator(NullLocator())
+    #     plt.gca().yaxis.set_major_locator(NullLocator())
+    #     filename = path.split("/")[-1].split(".")[0]
+    #     plt.savefig(f"output/{filename}.png", bbox_inches="tight", pad_inches=0.0)
+    #     plt.close()
+
+    # with open("output/result.txt", "wb") as f:
+    #     pickle.dump(inference_dict, f)
